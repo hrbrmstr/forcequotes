b7af04ff92567fae3393ac6068c9df5a888c51a4
hrbrmstr <bob@rud.is>
2019-01-12 15:24:02 -0500

    R package repo initialization complete

---
+++ R/forcequotes-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/forcequotes>
+#' - BugReports: <https://gitlab.com/hrbrmstr/forcequotes/issues>
+#' 
+#' @md
+#' @name forcequotes
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
